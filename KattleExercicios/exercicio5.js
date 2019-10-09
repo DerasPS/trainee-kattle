ccb560346a2d20a2275c5fe9f5596ff82e10a6b4
Acácio Pereira <acacioplopesjorge@hotmail.com>
2019-10-09 16:54:50 -0300

    Exercício 5

---
+++ KattleExercicios/exercicio5.js
@@ -0,0 +1,22 @@
+
+function exibiHabilidades (usuarios){
+    for ( usuarios of usuarios){
+        console.log('O ' +
+        usuarios.nome +
+        ' possui as habilidades: ' +
+        usuarios.habilidades.join(", ")
+        )
+    }
+}
+
+var usuarios = [  
+    {   
+        nome: "Diego",    
+        habilidades: ["Javascript", "ReactJS", "Redux"]  
+    },  
+    {    
+        nome: "Gabriel",    
+        habilidades: ["VueJS", "Ruby on Rails", "Elixir"]  
+    } 
+];
+exibiHabilidades(usuarios)
