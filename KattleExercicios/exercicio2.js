064e72dc41c41c2e439283d81824c02a09e62934
Acácio Pereira <acacioplopesjorge@hotmail.com>
2019-10-09 16:54:07 -0300

    Exercício 2

---
+++ KattleExercicios/exercicio2.js
@@ -0,0 +1,10 @@
+function pares(x, y) {
+    for (var par = x; par <= y; par++){
+        if( par % 2 === 0){
+            console.log(par)
+        }
+    }
+    return console.log(pares.par)
+}
+
+console.log(pares(32, 321))
